65f0538666c0ba08007df8788890f2b31f6b6748
peffjepin <peffjepin@gmail.com>
2023-01-04 02:27:33 -0500

    added padding and fill options to Line

--- test/test_line.py
+++ test/test_line.py
@@ -4,13 +4,40 @@ from tuindow import Line
 
 
 @params(
-    "line, expected",
-    (Line(10), ""),
-    (Line(10, data="abc"), "abc"),
+    "line,expected",
+    (Line(3, data="abc"), "abc"),
     (Line(2, data="abc"), "ab"),
+    (Line(2), "  "),
+    (Line(3, data="ab"), "ab "),
+    (Line(3, data="ab", fill="."), "ab."),
+    (Line(5, data="abcde", padding=(1, 0)), " abcd"),
+    (Line(5, data="abcde", padding=(0, 1)), "abcd "),
+    (Line(5, data="abcde", padding=(2, 1)), "  ab "),
 )
 def test_display(line, expected):
     assert line.display == expected
+
+
+@params(
+    "attr,value",
+    ("fill", "!"),
+    ("data", "abc"),
+    ("length", 2),
+    ("padding", (1, 2)),
+)
+def test_dirty_on_state_change(attr, value):
+    ln = Line(10)
+    ln.dirty = False
+
+    setattr(ln, attr, value)
+    assert ln.dirty
+
+
+def test_data_is_anything_that_implements_str():
+    class Data:
+        def __str__(self):
+            return "txt"
+    assert Line(3, data=Data()).display == "txt"
 
 
 @params("length", 0, -1)
@@ -26,21 +53,31 @@ def test_modifying_length_less_than_1(expect_error, length):
         ln.length = length
 
 
+def test_fill():
+    assert Line(10, fill="!").display == "!"*10
+
+
+def test_default_fill_is_space():
+    assert Line(10).display == " "*10
+
+
+@params("fill", "", "--")
+def test_fill_must_be_length_1(expect_error, fill):
+    with expect_error(ValueError, "fill", "length 1", fill):
+        Line(10, fill=fill)
+
+
 def test_dirty_on_init():
     assert Line(1).dirty
 
 
-def test_dirty_on_length_change():
-    ln = Line(1)
-    ln.dirty = False
-
-    ln.length = 2
-    assert ln.dirty
-
-
-def test_dirty_on_data_change():
-    ln = Line(1)
-    ln.dirty = False
-
-    ln.data = "abc"
-    assert ln.dirty
+@params(
+    "padding,length",
+    ((1, 1), 1),
+    ((2, 0), 1),
+    ((0, 2), 1),
+    ((1, 1), 2),
+)
+def test_padding_exceeds_length(expect_error, padding, length):
+    with expect_error(ValueError, "padding", "length", repr(length), repr(padding)):
+        Line(length, padding=padding)
--- tuindow/buffers.py
+++ tuindow/buffers.py
@@ -1,5 +1,6 @@
 from typing import List
 from typing import Iterator
+from typing import Tuple
 
 
 class Line:
@@ -7,12 +8,51 @@ class Line:
 
     _data: str
     _length: int
+    _display_capacity: int
+    _fill: str
+    _padding: Tuple[int, int]
+    _left_pad: str = ""
+    _right_pad: str = ""
 
-    def __init__(self, length: int, data="") -> None:
-        if length < 1:
-            raise ValueError(f"Line {length=} must be greater than 0")
+    def __init__(self, length: int, data="", fill=" ", padding=(0, 0)) -> None:
+        self.fill = fill
         self.length = length
         self.data = data
+        self.dirty = True
+        self.padding = padding
+
+    @property
+    def padding(self) -> Tuple[int, int]:
+        return self._padding
+
+    @padding.setter
+    def padding(self, value: Tuple[int, int]) -> None:
+        if sum(x for x in value if x > 0) >= self.length:
+            raise ValueError(
+                f"Line padding ({value=!r}) exceeded length ({self.length!r})")
+
+        self._display_capacity = self.length
+        self._left_pad = ""
+        self._right_pad = ""
+        if value[0] >= 0:
+            self._left_pad = self.fill*value[0]
+            self._display_capacity -= value[0]
+        if value[1] >= 0:
+            self._right_pad = self.fill*value[1]
+            self._display_capacity -= value[1]
+        self._padding = value
+        self.dirty = True
+
+    @property
+    def fill(self) -> str:
+        return self._fill
+
+    @fill.setter
+    def fill(self, value: str) -> None:
+        if len(value) != 1:
+            raise ValueError(
+                f"Line fill ({value=!r}) must be a string of length 1")
+        self._fill = value
         self.dirty = True
 
     @property
@@ -31,13 +71,21 @@ class Line:
     @length.setter
     def length(self, value: int) -> None:
         if value < 1:
-            raise ValueError(f"Line length {value=} must be greater than 0")
+            raise ValueError(f"Line length ({value=}) must be greater than 0")
         self._length = value
         self.dirty = True
 
     @property
     def display(self) -> str:
-        return self.data[: self.length]
+        display = str(self.data)
+        if len(display) < self._display_capacity:
+            return (
+                self._left_pad +
+                display +
+                self.fill*(self._display_capacity - len(display)) +
+                self._right_pad
+            )
+        return self._left_pad + display[:self._display_capacity] + self._right_pad
 
 
 class Panel:
@@ -66,47 +114,47 @@ class Panel:
     def __iter__(self) -> Iterator[Line]:
         return iter(self.lines[:self.height])
 
-    @ property
+    @property
     def x(self) -> int:
         return self._x
 
-    @ x.setter
+    @x.setter
     def x(self, value: int) -> None:
         if value < 0:
-            raise ValueError(f"Panel x {value=} cannot be negative")
+            raise ValueError(f"Panel x ({value=}) cannot be negative")
         self._x = value
 
-    @ property
+    @property
     def y(self) -> int:
         return self._y
 
-    @ y.setter
+    @y.setter
     def y(self, value: int) -> None:
         if value < 0:
-            raise ValueError(f"Panel y {value=} cannot be negative")
+            raise ValueError(f"Panel y ({value=}) cannot be negative")
         self._y = value
 
-    @ property
+    @property
     def width(self) -> int:
         return self._width
 
-    @ width.setter
+    @width.setter
     def width(self, value: int) -> None:
         if value < 1:
-            raise ValueError(f"Panel width {value=} must be greater than 0")
+            raise ValueError(f"Panel width ({value=}) must be greater than 0")
         if value != self._width and self.lines:
             for line in self.lines:
                 line.length = value
         self._width = value
 
-    @ property
+    @property
     def height(self) -> int:
         return self._height
 
-    @ height.setter
+    @height.setter
     def height(self, value: int) -> None:
         if value < 1:
-            raise ValueError(f"Panel height {value=} must be greater than 0")
+            raise ValueError(f"Panel height ({value=}) must be greater than 0")
         if value > self._height:
             while value > len(self.lines):
                 self.lines.append(Line(self.width))
